c7cedcdfc1142dc51d1d7041602506b1741971dd
maryamiqtidar <128498038+maryamiqtidar@users.noreply.github.com>
2024-02-05 18:06:55 +0500

    Delete store directory

--- store/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from .models import Order
-
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['customer_name', 'customer_email', 'customer_mobile', 'required_size', 'quantity']
--- store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, order_list, order_detail
-
-urlpatterns = [
-    path('', index, name='index'),  # URL for the index view
-    path('orders/', order_list, name='order-list'),  # URL for listing orders and creating a new order
-    path('orders/<int:pk>/', order_detail, name='order-detail'),  # URL for updating and deleting a specific order
-]
